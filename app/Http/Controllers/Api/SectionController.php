72b28692c088fd982ff448a28e4e2b519dcfeee6
Vaibhav Patil <vaibhav.p@extraaazpos.com>
2024-03-15 19:10:09 +0530

    commitaniket

---
+++ app/Http/Controllers/Api/SectionController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Section;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        //
+        $user = Auth::user();
+
+        $restaurant_id  = $request->input('restaurant_id');
+
+        $data1 = User::where('id', $user->id)->get()->toArray();
+
+        $section = Section::where('restaurant_id', $user->restaurant_id)->get();
+
+        return response()->json(["success" => true, "data" => $section]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+         $user = Auth::user();
+        // $data1 = User::where('id', $user->id)->get()->toArray(); // Corrected $user->Id to $user->id
+        $restaurant_id = $user->restaurant_id;
+        $order = new Order();
+        $order->id   = $request->id;
+        $order->ispaid = $request->ispaid;
+        $order->table_number  = $request->table_number;
+        $order->floor_number = $request->floor_number;
+        $order->save();
+        return response()->json(['success' => true, 'message' => 'order added successfully', 'data' => $order]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
