f82b0fccd40cd3815dd0e16f493323eb2d917cf3
Vaibhav Patil <vaibhav.p@extraaazpos.com>
2024-03-29 19:51:48 +0530

    commtitie

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -95,6 +95,7 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    
     public function destroy($id)
     {
         $category = Category::findOrFail($id);
